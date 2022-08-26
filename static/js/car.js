d3e19e67ff882c94dea55b63f153921b8b3edbcd
Yang Song <entpcoder@gmail.com>
2022-08-26 18:53:36 +0800

    购物车页面

--- static/js/car.js
+++ static/js/car.js
@@ -1,2 +1,16 @@
-$(document).ready(function(){
+$(".order-button-del").bind("click",function(){
+    layer.msg('你确定扔出购物车么？', {
+        time: 0 //不自动关闭
+        ,btn: ['必须啊', '我还要']
+        ,yes: function(index){
+          layer.close(index);
+          layer.msg('删除操作');
+        }
+    });
+})
+$("#st-tips").bind("mouseover",function(){
+  layer.tips('好商品需要精挑细选，特意为你留足20分钟考虑时间。', '#st-tips-text', {
+    tips: [3, '#3595CC'],
+    time: 3000
+  });
 })
